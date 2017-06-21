9dfe6990e3412096788187509ab9b2995e19ba87
muyangren2 <624376142@qq.com>
2017-06-21 09:26:24 +0800

    some message for this project.

---
+++ src/com/coddy/hjj/Hello_codyy.java
@@ -0,0 +1,14 @@
+package com.coddy.hjj;
+
+public class Hello_codyy {
+
+	public Hello_codyy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello moring");
+
+	}
+
+}
